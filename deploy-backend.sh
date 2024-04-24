9e090561d1a0643667b08116a10b2eaae750b286
Øyvind <oyvindj@stacc.com>
2024-04-24 20:37:49 +0200

    deployment from local

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -4,4 +4,4 @@ git init
 heroku git:remote -a my-fridge-backend
 git add .
 git commit -am "deployment from local"
-git subtree push --prefix my_fridge_backend heroku master
+git subtree push --prefix my_fridge_backend heroku master -f
