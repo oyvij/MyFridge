5560f486910f48c242c8e6d54fed48040578925f
Øyvind <oyvindj@stacc.com>
2024-05-11 02:22:50 +0200

    deployment from local

--- deploy-flutter.sh
+++ deploy-flutter.sh
@@ -2,5 +2,4 @@
 heroku git:remote -a my-fridge-flutter
 git add .
 git commit -am "deployment from local"
-git subtree push --prefix my_fridge_flutter heroku master
-heroku ps:scale web=1
+git subtree push --prefix my_fridge_flutter heroku main
