9d674a54ea21d8449a5de5f37e07c06893099164
Øyvind <oyvindj@stacc.com>
2024-05-11 18:25:05 +0200

    fix: yea

--- buildpack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf ./my_fridge_backend/src/public
-
-# Step 1: Build Flutter web app
-(cd ./my_fridge_flutter && flutter build web)
-
-# Step 2: Copy build folder to backend directory
-cp -R ./my_fridge_flutter/build ./my_fridge_backend/src/public
