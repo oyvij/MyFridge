97542ffb985c84d777affbf9108260097a80410f
Øyvind <oyvindj@stacc.com>
2024-05-11 05:33:56 +0200

    deployment from local

--- my_fridge_backend/src/index.js
+++ my_fridge_backend/src/index.js
@@ -24,29 +24,10 @@ config.kassalClient = kassalClient(process.env.KASSAL_API_KEY)
 // Serve static files from the build directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a function to recursively traverse directories and create routes
-function createRoutes(dir, prefix = '/') {
-    // Get the list of files and directories in the current directory
-    const entries = fs.readdirSync(dir, { withFileTypes: true });
-
-    // Iterate over each entry
-    entries.forEach(entry => {
-        const fullPath = path.join(dir, entry.name);
-
-        // If the entry is a directory, recursively create routes
-        if (entry.isDirectory()) {
-            const subPrefix = path.join(prefix, entry.name);
-            createRoutes(fullPath, subPrefix);
-        }
-    });
-}
-
-// Root directory containing static files
-const rootDir = path.join(__dirname, 'public', 'build', 'web');
-
-// Create routes for all directories and files within the root directory
-createRoutes(rootDir);
+// Handle requests to the root URL
+app.use('/', express.static(path.join(__dirname, 'public', 'build', 'web')));
 
 app.use('/api', api({ config }));
+
 app.server.listen(process.env.PORT);
 console.log(`Started on port ${process.env.PORT}`);
