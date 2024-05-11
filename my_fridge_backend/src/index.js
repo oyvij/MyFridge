2d8da18a74a2bf480686ea92ad5cb4562d98178e
Øyvind <oyvindj@stacc.com>
2024-05-11 05:22:57 +0200

    deployment from local

--- my_fridge_backend/src/index.js
+++ my_fridge_backend/src/index.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 import api from './api';
 import config from './config.json';
 import kassalClient from './clients/kassalClient';
+import e from 'express';
 const path = require('path');
 require('dotenv').config();
 
@@ -24,12 +25,22 @@ config.kassalClient = kassalClient(process.env.KASSAL_API_KEY)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Handle requests to the root URL
-app.get('/', (req, res) => {
-    // Serve the index.html file from the build directory
-    res.sendFile(path.join(__dirname, 'public', 'build', 'web', 'index.html'));
-});
+app.use('/', express.static(path.join(__dirname, 'public', 'build', 'web')));
 
-app.use('/', express.static(path.join(__dirname, 'public', 'build', 'web')))
+/* app.use('/flutter.js', express.static(path.join(__dirname, 'public', 'build', 'web', 'flutter.js')))
+
+app.use('/flutter_service_worker.js', express.static(path.join(__dirname, 'public', 'build', 'web', 'flutter_service_worker.js')));
+
+app.use('/main.dart.js', express.static(path.join(__dirname, 'public', 'build', 'web', 'main.dart.js')));
+
+app.use('/main.dart.js.map', express.static(path.join(__dirname, 'public', 'build', 'web', 'main.dart.js.map')));
+
+app.use('/favicon.png', express.static(path.join(__dirname, 'public', 'build', 'web', 'favicon.png')));
+
+
+app.use('/favicon.ico', express.static(path.join(__dirname, 'public', 'build', 'web', 'favicon.ico')))
+
+app.use('/manifest.json', express.static(path.join(__dirname, 'public', 'build', 'web', 'manifest.json'))) */
 
 app.use('/icons', express.static(path.join(__dirname, 'public', 'build', 'web', 'icons')))
 
