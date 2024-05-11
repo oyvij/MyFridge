82ff8a662c8ba4f8f3998e1e40eb5dd490f25f9e
Øyvind <oyvindj@stacc.com>
2024-05-11 03:57:05 +0200

    deployment from local

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+heroku buildpacks:clear
+heroku buildpacks:set https://github.com/timanovsky/subdir-heroku-buildpack
+heroku buildpacks:add heroku/nodejs
+heroku config:set PROJECT_PATH=my_fridge_backend
 heroku git:remote -a my-fridge-backend
 git add .
 git commit -am "deployment from local"
