b016a1e9e08f40170301cb63bf97f86544b44816
Øyvind <oyvindj@stacc.com>
2024-05-11 03:37:58 +0200

    deployment from local

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd my_fridge_flutter && flutter build web --release
 cd ../
-cp -r my_fridge_flutter/build/web my_fridge_backend/build
+cp -r my_fridge_flutter/build/web my_fridge_backend/src/build
 heroku git:remote -a my-fridge-backend
 git add .
 git commit -am "deployment from local"
